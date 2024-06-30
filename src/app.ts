33c3b94615e45f223f375bcceb123b66d091a941
rldnrl <devpark1002@gmail.com>
2024-07-01 00:43:13 +0900

    feat: connect todo routes with express

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
+import todoRoutes from "./todo/routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/todos", console.log);
-app.post("/todos", console.log);
-app.patch("/todos", console.log);
+app.use("/api", todoRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
