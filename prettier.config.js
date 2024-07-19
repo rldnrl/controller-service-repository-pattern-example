743fa86183467b0c17eed4da4416407e661f1e07
rldnrl <devpark1002@gmail.com>
2024-07-19 20:49:54 +0900

    feat: set up prettier

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: "lf",
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+};
