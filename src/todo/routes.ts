7e5e4c0ee2b75df754fd18be5041d8cfc20e034f
rldnrl <devpark1002@gmail.com>
2024-07-19 22:08:00 +0900

    feat: prettier 적용

--- src/todo/routes.ts
+++ src/todo/routes.ts
@@ -7,7 +7,7 @@ const todoController = new TodoController();
 router.get("/todos", (req, res) => todoController.getAllTodos(req, res));
 router.post("/todos", (req, res) => todoController.createTodo(req, res));
 router.patch("/todos/:id", (req, res) =>
-  todoController.updateTodoStatus(req, res),
+  todoController.updateTodoStatus(req, res)
 );
 
 export default router;
