163d9e6737667ac68e54e6764f79059c2f2d9c23
rldnrl <devpark1002@gmail.com>
2024-07-19 19:39:27 +0900

    feat: add router about createTodo controller

--- src/todo/routes.ts
+++ src/todo/routes.ts
@@ -5,5 +5,6 @@ const router = Router();
 const todoController = new TodoController();
 
 router.get("/todos", (req, res) => todoController.getAllTodos(req, res));
+router.post("/todos", (req, res) => todoController.createTodo(req, res));
 
 export default router;
