385df086aafad0e5a9746abbe910477cd0a05d7b
rldnrl <devpark1002@gmail.com>
2024-07-19 19:42:30 +0900

    feat: add router about updateStatusTodo controller

--- src/todo/routes.ts
+++ src/todo/routes.ts
@@ -6,5 +6,8 @@ const todoController = new TodoController();
 
 router.get("/todos", (req, res) => todoController.getAllTodos(req, res));
 router.post("/todos", (req, res) => todoController.createTodo(req, res));
+router.patch("/todos/:id", (req, res) =>
+  todoController.updateTodoStatus(req, res),
+);
 
 export default router;
